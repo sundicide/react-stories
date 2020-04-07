4aa93778b5929f6d4e92be064043b458b6c2863c
GueSeon Jeong <gueseon.jeong@standigm.com>
2020-04-07 12:09:08 +0900

    add color story

---
+++ src/stories/colors/colors.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "grid",
+    gridAutoFlow: "row",
+    gridRowGap: 20
+  },
+  itemWrapper: {
+    border: "1px solid black",
+    padding: 5
+  },
+  itemtitle: {
+    fontSize: "2rem"
+  },
+  item: {
+    display: "grid",
+    gridTemplateColumns: "200px 200px"
+  }
+}));
+
+export default {
+  title: 'Colors'
+};
+
+export const Colors = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.itemWrapper}>
+        <div className={classes.itemtitle}>Material UI</div>
+        <div className={classes.item}>
+          <div className={classes.subtitle}> Selection Color </div>
+          <div style={{backgroundColor: "rgb(253, 224, 234)"}}></div>
+        </div>
+      </div>
+      <div className={classes.itemWrapper}>
+        <div className={classes.itemtitle}>Material Table</div>
+        <div className={classes.item}>
+          <div className={classes.subtitle}> Selection Color </div>
+          <div style={{backgroundColor: "rgb(253, 224, 234)"}}></div>
+        </div>
+      </div>
+    </div>
+  )
+}
